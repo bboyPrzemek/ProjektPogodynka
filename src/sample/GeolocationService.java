2be02c0f1589ae6d4b2d84b5feeaaf4c55493960
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-12-10 22:12:09 +0100

    zmiana folderow bibliotek

--- src/sample/GeolocationService.java
+++ src/sample/GeolocationService.java
@@ -14,31 +14,28 @@ public class GeolocationService {
     private static String LOCATION_URL = "http://api.ipstack.com/check?access_key=";
     private static String LOCATION_API_KEY = "f6615b8da04b3c16a49a895974104e67";
 
-        public static String getLocation() {
-            try {
-                URL url = new URL(LOCATION_URL+LOCATION_API_KEY);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-                    conn.setRequestProperty("Accept", "application/json");
-                    BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-                    String output;
-
-                    StringBuilder sb = new StringBuilder();
-                    while ((output = br.readLine()) != null) {
-                        System.out.println(output);
-                        sb.append(output);
-                    }
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    LocationWrapper locationWrapper = objectMapper.readValue(sb.toString(), LocationWrapper.class);
-                    conn.disconnect();
-                    System.out.println(locationWrapper.getCity());
-                    return  locationWrapper.getCity();
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
+    public static String getLocation() {
+        try {
+            URL url = new URL(LOCATION_URL + LOCATION_API_KEY);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Accept", "application/json");
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            StringBuilder sb = new StringBuilder();
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                sb.append(output);
             }
+            ObjectMapper objectMapper = new ObjectMapper();
+            LocationWrapper locationWrapper = objectMapper.readValue(sb.toString(), LocationWrapper.class);
+            conn.disconnect();
+            return locationWrapper.getCity();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+}
