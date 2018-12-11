dbcf3d7e506bc7a32fea235adb68686d5277d65a
Wojtek <34339517+wojtekkandziak@users.noreply.github.com>
2018-12-11 21:25:08 +0100

    GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pogodynka");
-        primaryStage.setScene(new Scene(root, 300, 600));
+        primaryStage.setScene(new Scene(root, 290, 590));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
